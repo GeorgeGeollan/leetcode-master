5926c67207a648c222c3a68a1a50725fe0f8c424
Geollan <GeorgeGeollan@gmail.com>
2024-01-16 17:56:47 +0800

    feat: 剑指Offer24题解修改

--- AimAtOffer/src/main/org/offer/geollan/jz24/Solution01.java
+++ AimAtOffer/src/main/org/offer/geollan/jz24/Solution01.java
@@ -1,20 +1,20 @@
 package main.org.offer.geollan.jz24;
 
 /**
- * @Description
+ * @Description 迭代法，双指针 时间复杂度O(n) 空间复杂度O(1)
  * @Date: 2024/1/16 16:27
  * @Author: Geollan
  **/
-class ListNode {
-    int val;
-    ListNode next = null;
-
-    public ListNode(int val) {
-        this.val = val;
-    }
-}
 
 public class Solution01 {
+    class ListNode {
+        int val;
+        ListNode next = null;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
 
     public ListNode ReverseList(ListNode head) {
         // 记录当前节点的前一个节点
