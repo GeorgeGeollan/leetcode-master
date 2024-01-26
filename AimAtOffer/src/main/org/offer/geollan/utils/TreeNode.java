d7c5745aa10a1a87ef2ecb32c1eca028f5c1fdd4
Geollan <GeorgeGeollan@gmail.com>
2024-01-26 14:38:55 +0800

    feat: TreeNode新增

---
+++ AimAtOffer/src/main/org/offer/geollan/utils/TreeNode.java
@@ -0,0 +1,16 @@
+package main.org.offer.geollan.utils;
+
+/**
+ * @Description
+ * @Date: 2024/1/26 14:12
+ * @Author: Geollan
+ **/
+public class TreeNode {
+    public int val = 0;
+    public TreeNode left = null;
+    public TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
