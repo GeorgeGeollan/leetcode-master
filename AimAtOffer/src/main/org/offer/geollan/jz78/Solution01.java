aaed69ee915c6e56416f94b9f304cfc5674c0b38
Geollan <GeorgeGeollan@gmail.com>
2024-02-10 23:14:37 +0800

    feat:剑指Offer78新增题解

---
+++ AimAtOffer/src/main/org/offer/geollan/jz78/Solution01.java
@@ -0,0 +1,42 @@
+package main.org.offer.geollan.jz78;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import main.org.offer.geollan.utils.TreeNode;
+
+/**
+ * @Description
+ * @Date: 2024/2/10 23:13
+ * @Author: Geollan
+ **/
+public class Solution01 {
+    public ArrayList<ArrayList<Integer>> Print(TreeNode pRoot) {
+        // write code here
+        if (pRoot == null)
+            return new ArrayList<>();
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(pRoot);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            ArrayList<Integer> level = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode root = queue.poll();
+                level.add(root.val);
+
+                if (root.left != null)
+                    queue.add(root.left);
+                if (root.right != null)
+                    queue.add(root.right);
+            }
+
+            ans.add(level);
+        }
+
+        return ans;
+    }
+}
