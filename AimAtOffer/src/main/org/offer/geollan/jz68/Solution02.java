99f2d275c0bb51ca06ab46c3289f70986f82dae4
Geollan <GeorgeGeollan@gmail.com>
2024-02-12 21:50:55 +0800

    feat:剑指Offer68新增题解

---
+++ AimAtOffer/src/main/org/offer/geollan/jz68/Solution02.java
@@ -0,0 +1,24 @@
+package main.org.offer.geollan.jz68;
+
+import main.org.offer.geollan.utils.TreeNode;
+
+/**
+ * @Description 递归 时间复杂度O(N) 空间复杂度O(1)
+ * @Date: 2024/2/12 21:50
+ * @Author: Geollan
+ **/
+public class Solution02 {
+    public int lowestCommonAncestor(TreeNode root, int p, int q) {
+        // write code here
+        while (root != null) {
+            if (root.val > p && root.val > q)
+                root = root.left;
+            if (root.val < p && root.val < q)
+                root = root.right;
+            else
+                break;
+        }
+
+        return root == null ? -1 : root.val;
+    }
+}
