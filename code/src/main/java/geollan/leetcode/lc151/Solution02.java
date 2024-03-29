6111c4c7ab3474ae13a7478051e7bd818016a846
Geollan <GeorgeGeollan@gmail.com>
2024-03-29 11:17:55 +0800

    feat:力扣151修改注释

--- code/src/main/java/geollan/leetcode/lc151/Solution02.java
+++ code/src/main/java/geollan/leetcode/lc151/Solution02.java
@@ -7,14 +7,14 @@ package geollan.leetcode.lc151;
  **/
 public class Solution02 {
     public String reverseWords(String s) {
-        s = s.trim();                                    // 删除首尾空格
+        s = s.trim();
         int j = s.length() - 1, i = j;
         StringBuilder sb = new StringBuilder();
 
         while(i >= 0) {
             while (i >= 0 && s.charAt(i) != ' ') i--;
             sb.append(s.substring(i + 1, j + 1) + " ");
-            while (i >= 0 && s.charAt(i) == ' ') i--;     // 跳过单词间空格
+            while (i >= 0 && s.charAt(i) == ' ') i--;
             j = i;
         }
 
