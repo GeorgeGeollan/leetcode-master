a2e93412e6d21ba17d8395f8c4239aa46a35d745
Geollan <GeorgeGeollan@gmail.com>
2024-04-01 09:11:48 +0800

    feat:力扣2810新增题解

---
+++ code/src/main/java/geollan/leetcode/lc2810/Solution01.java
@@ -0,0 +1,20 @@
+package geollan.leetcode.lc2810;
+
+/**
+ * @Description StringBuilder 时间复杂度O(N) 空间复杂度O(N)
+ * @Date: 2024/4/1 09:06
+ * @Author: Geollan
+ **/
+public class Solution01 {
+    public String finalString(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == 'i') sb.reverse();
+
+            else sb.append(s.charAt(i));
+        }
+
+        return sb.toString();
+    }
+}
