47bffeaabe91bf846049d4317137cf2dbce5f1d3
Geollan <GeorgeGeollan@gmail.com>
2024-04-04 11:59:24 +0800

    feat: 力扣392新增题解

---
+++ code/src/main/java/geollan/leetcode/lc392/Solution01.java
@@ -0,0 +1,24 @@
+package geollan.leetcode.lc392;
+
+/**
+ * @Description 双指针 时间复杂度O(N) 空间复杂度O(N)
+ * @Date: 2024/4/3 11:10
+ * @Author: Geollan
+ **/
+public class Solution01 {
+    public boolean isSubsequence(String s, String t) {
+        int m = s.length(), n = t.length();
+        int i = 0, j = 0;
+
+        while(i < m && j < n) {
+            if(s.charAt(i) == t.charAt(j)) {
+                i++;
+                j++;
+            }
+
+            else j++;
+        }
+
+        return i == m;
+    }
+}
