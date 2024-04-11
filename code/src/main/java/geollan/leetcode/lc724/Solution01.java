13f339db297eb39065cdda10f11aa814d103cffb
Geollan <GeorgeGeollan@gmail.com>
2024-04-11 10:17:49 +0800

    feat: 力扣724新增题解

---
+++ code/src/main/java/geollan/leetcode/lc724/Solution01.java
@@ -0,0 +1,46 @@
+package geollan.leetcode.lc724;
+
+/**
+ * @Description
+ * @Date: 2024/4/11 10:05
+ * @Author: Geollan
+ **/
+public class Solution01 {
+    public int pivotIndex(int[] nums) {
+        int rightSum = 0, leftSum = 0, sum = 0, idx = -1;
+
+        for(int num: nums) {
+            sum += num;
+        }
+
+        rightSum = sum - leftSum;
+
+        for(int i = 0; i < nums.length; i++) {
+            rightSum -= nums[i];
+            if(i != 0) leftSum += nums[i - 1];
+            System.out.println(leftSum + " " +rightSum);
+            if(rightSum == leftSum) return i;
+        }
+
+        return idx;
+    }
+
+    // 优化版本
+    public int pivotIndexPlus(int[] nums) {
+        int rightSum = 0, leftSum = 0, sum = 0, idx = -1;
+
+        for(int num: nums) {
+            sum += num;
+        }
+
+        rightSum = sum;
+
+        for(int i = 0; i < nums.length; i++) {
+            rightSum -= nums[i];
+            if(rightSum == leftSum) return i;
+            leftSum += nums[i];
+        }
+
+        return idx;
+    }
+}
