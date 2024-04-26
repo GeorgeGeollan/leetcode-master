3a0c01ec9b7a3a9f868ec6c1e59f46694edf7205
Geollan <GeorgeGeollan@gmail.com>
2024-04-26 09:16:39 +0800

    feat:力扣206新增题解

---
+++ code/src/main/java/geollan/leetcode/lc206/Solution01.java
@@ -0,0 +1,25 @@
+package geollan.leetcode.lc206;
+
+import geollan.utils.ListNode;
+
+/**
+ * @Description 定义一个指针，从头开始反转 时间复杂度O(N) 空间复杂度O(1)
+ * @Date: 2024/4/26 9:02
+ * @Author: Geollan
+ **/
+public class Solution01 {
+    public ListNode reverseList(ListNode head) {
+        ListNode ptr = null;
+        ListNode prev = null;
+
+        while(head != null) {
+            prev = head;
+            ListNode tmp = head.next;
+            head.next = ptr;
+            ptr = head;
+            head = tmp;
+        }
+
+        return prev;
+    }
+}
