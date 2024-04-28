7f8f225c620d7a3f883747a99c280f2a7abe8343
Geollan <GeorgeGeollan@gmail.com>
2024-04-28 08:59:45 +0800

    feat:力扣2130新增题解

---
+++ code/src/main/java/geollan/leetcode/lc2130/Solution01.java
@@ -0,0 +1,29 @@
+package geollan.leetcode.lc2130;
+
+import geollan.utils.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description
+ * @Date: 2024/4/28 8:59
+ * @Author: Geollan
+ **/
+public class Solution01 {
+    public int pairSum(ListNode head) {
+        List<Integer> num = new ArrayList<>();
+        int max = 0;
+
+        while(head != null) {
+            num.add(head.val);
+            head = head.next;
+        }
+
+        for(int i = 0; i < num.size() / 2; i++) {
+            max = Math.max(max, num.get(i) + num.get(num.size() - 1 - i));
+        }
+
+        return max;
+    }
+}
