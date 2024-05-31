43155ccec6e39abf6e4fdfb42d9f926d83927751
Geollan <GeorgeGeollan@gmail.com>
2024-05-31 10:03:13 +0800

    feat:力扣9新增题解

---
+++ code/src/main/java/geollan/leetcode/lc09/Solution01.java
@@ -0,0 +1,24 @@
+package geollan.leetcode.lc09;
+
+/**
+ * @Description
+ * @Date: 2024/5/31 10:02
+ * @Author: Geollan
+ **/
+public class Solution01 {
+    public boolean isPalindrome(int x) {
+        String str = String.valueOf(x);
+        int n = str.length();
+
+        int l = 0, r = n - 1;
+
+        while(l < r) {
+            if(str.charAt(l) != str.charAt(r)) return false;
+
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+}
