a04d7073d5700b17b07968c6ed8673ebf893eb54
Geollan <GeorgeGeollan@gmail.com>
2024-06-07 10:05:09 +0800

    feat:力扣3168新增题解

---
+++ code/src/main/java/geollan/leetcode/lc3168/Solution02.java
@@ -0,0 +1,21 @@
+package geollan.leetcode.lc3168;
+
+/**
+ * @Description 单独变量 时间复杂度O(N) 空间复杂度O(1)
+ * @Date: 2024/6/7 10:00
+ * @Author: Geollan
+ **/
+public class Solution02 {
+    public int minimumChairs(String s) {
+        int ans = 0;
+        int cnt = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == 'E') ans = Math.max(ans, ++cnt);
+
+            else cnt--;
+        }
+
+        return ans;
+    }
+}
