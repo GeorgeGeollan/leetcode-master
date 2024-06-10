ef00e319b37a81b1c3f7ba783641de9f58a21e5a
Geollan <GeorgeGeollan@gmail.com>
2024-06-10 12:31:03 +0800

    feat:力扣3169新增题解

---
+++ code/src/main/java/geollan/leetcode/lc3169/Solution01.java
@@ -0,0 +1,27 @@
+package geollan.leetcode.lc3169;
+
+import java.util.Arrays;
+
+/**
+ * @Description 时间复杂度O(logN) 空间复杂度O(1)
+ * @Date: 2024/6/10 12:28
+ * @Author: Geollan
+ **/
+public class Solution01 {
+    public int countDays(int days, int[][] meetings) {
+        Arrays.sort(meetings, (p, q) -> (p[0] - q[0]));
+        int start = 1, end = 0;
+
+        for(int[] p: meetings) {
+            if(p[0] > end) {
+                days -= end - start + 1;
+                start = p[0];
+            }
+
+            end = Math.max(end, p[1]);
+        }
+
+        days -= end - start + 1;
+        return days;
+    }
+}
