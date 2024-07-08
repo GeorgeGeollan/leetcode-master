e82c21b560e62a681778b4233795647daea6a9e4
Geollan <GeorgeGeollan@gmail.com>
2024-07-08 11:07:32 +0800

    feat:力扣3200新增题解

---
+++ code/src/main/java/geollan/leetcode/lc3200/Solution01.java
@@ -0,0 +1,17 @@
+package geollan.leetcode.lc3200;
+
+/**
+ * @Description 时间复杂度O(N) 空间复杂度O(1)
+ * @Date: 2024/7/8 11:07
+ * @Author: Geollan
+ **/
+public class Solution01 {
+    public int maxHeightOfTriangle(int red, int blue) {
+        int[] cnt = new int[2];
+        for(int i = 1; ; i++) {
+            cnt[i % 2] += i;
+
+            if((cnt[0] > red || cnt[1] > blue) && (cnt[0] > blue || cnt[1] > red)) return i - 1;
+        }
+    }
+}
