2f8c675d08a75ac63de8ec0fbae0b9a56ab54754
Geollan <GeorgeGeollan@gmail.com>
2024-07-23 14:53:16 +0800

    feat:力扣3226新增题解

---
+++ code/src/main/java/geollan/leetcode/lc3226/Solution01.java
@@ -0,0 +1,12 @@
+package geollan.leetcode.lc3226;
+
+/**
+ * @Description 位运算 时间复杂度O(1) 空间复杂度O(1)
+ * @Date: 2024/7/23 14:50
+ * @Author: Geollan
+ **/
+public class Solution01 {
+    public int minChanges(int n, int k) {
+        return (n & k) != k ? -1 : Integer.bitCount(n ^ k);
+    }
+}
