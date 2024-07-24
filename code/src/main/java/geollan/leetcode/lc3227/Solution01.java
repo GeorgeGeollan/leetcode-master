4342033adf3410e89cc86edeea2c3c9adce74ab0
Geollan <GeorgeGeollan@gmail.com>
2024-07-24 09:20:29 +0800

    feat:力扣3227新增题解

---
+++ code/src/main/java/geollan/leetcode/lc3227/Solution01.java
@@ -0,0 +1,16 @@
+package geollan.leetcode.lc3227;
+
+/**
+ * @Description 脑筋急转弯 时间复杂度O(N) 空间复杂度O(1)
+ * @Date: 2024/7/24 9:19
+ * @Author: Geollan
+ **/
+public class Solution01 {
+    public boolean doesAliceWin(String s) {
+        for(char c: s.toCharArray()) {
+            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') return true;
+        }
+
+        return false;
+    }
+}
