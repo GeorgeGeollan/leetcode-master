46053784c78242c60dbbbc8b1ccc3839dee04f2f
Geollan <GeorgeGeollan@gmail.com>
2024-07-26 09:36:19 +0800

    feat:力扣3222新增题解

---
+++ code/src/main/java/geollan/leetcode/lc3222/Solution02.java
@@ -0,0 +1,12 @@
+package geollan.leetcode.lc3222;
+
+/**
+ * @Description 时间复杂度O(1) 空间复杂度O(1)
+ * @Date: 2024/7/26 9:35
+ * @Author: Geollan
+ **/
+public class Solution02 {
+    public String losingPlayer(int x, int y) {
+        return Math.min(x, y / 4) % 2 == 1 ? "Alice" : "Bob";
+    }
+}
