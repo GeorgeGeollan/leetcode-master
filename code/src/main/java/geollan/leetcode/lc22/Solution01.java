9b3e0a1c61a8ef87e75bd4b016b4bb2b03d1538b
Geollan <GeorgeGeollan@gmail.com>
2024-08-09 09:42:39 +0800

    feat:力扣22新增题解

---
+++ code/src/main/java/geollan/leetcode/lc22/Solution01.java
@@ -0,0 +1,39 @@
+package geollan.leetcode.lc22;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description
+ * @Date: 2024/8/9 9:42
+ * @Author: Geollan
+ **/
+public class Solution01 {
+    private int n;
+    private char path[];
+    private List<String> ans = new ArrayList<>();
+    public List<String> generateParenthesis(int n) {
+        this.n = n;
+        path = new char[n * 2];
+        dfs(0, 0);
+
+        return ans;
+    }
+
+    public void dfs(int i, int open) {
+        if(i == n * 2) {
+            ans.add(new String(path));
+            return;
+        }
+
+        if(open < n) {
+            path[i] = '(';
+            dfs(i + 1, open + 1);
+        }
+
+        if(i - open < open) {
+            path[i] = ')';
+            dfs(i + 1, open);
+        }
+    }
+}
