0fe1752912cda300bb31bae93ccfc2cd533e3a8b
Geollan <GeorgeGeollan@gmail.com>
2025-03-15 09:30:54 +0800

    feat: 力扣31100新增题解

---
+++ code/src/geollan/leetcode/lc3100_3199/lc3110/Solution01.java
@@ -0,0 +1,18 @@
+package src.geollan.leetcode.lc3100_3199.lc3110;
+
+/**
+ * @Description
+ * @Date: 2025/3/15 9:30
+ * @Author: Geollan
+ **/
+public class Solution01 {
+    public int scoreOfString(String s) {
+        int score = 0;
+
+        for(int i = 0; i < s.length() - 1; i++) {
+            score += (Math.abs(s.charAt(i) -s.charAt(i + 1)));
+        }
+
+        return score;
+    }
+}
